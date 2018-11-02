930e3a08658ef574f044091db4beb984fd7b677e
Peter Kofler <peter.kofler@code-cop.org>
2018-11-02 08:24:59 +0100

    Add colours to test runner script

--- scheme/run_tests.bat
+++ scheme/run_tests.bat
@@ -1,4 +1,21 @@
-gsi test\assert-test.scm
-gsi test\cell-test.scm
-gsi test\grid-test.scm
-gsi test\grid-manager-test.scm
+@color 02
+call r5rs test\assert-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+call r5rs test\cell-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+call r5rs test\grid-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+call r5rs test\grid-manager-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+@echo test suite OK
+@goto :stop
+
+:fail
+@color 04
+@echo FAILED: !errorlevel!
+
+:stop
